32572b9dcdea84b1a173953ca3ed60710976dc01
William Woodhead <6097010+nofable@users.noreply.github.com>
2025-10-15 21:25:27 +0100

    rendering the full grid of 30 square tiles in white on black

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use bevy::prelude::*;
 
+const TILE_SIZE: f32 = 100.0;
+const TILE_MARGIN: f32 = 5.0;
+const TRANSLATION_OFFSET_X: f32 = (TILE_SIZE * 2.5) + (TILE_MARGIN * 2.0);
+const TRANSLATION_OFFSET_Y: f32 = (TILE_SIZE * 2.5) + (TILE_MARGIN * 2.0);
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
@@ -13,8 +18,43 @@ fn setup(
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     commands.spawn(Camera2d);
-    commands.spawn((
-        Mesh2d(meshes.add(Circle::new(50.0))),
-        MeshMaterial2d(materials.add(ColorMaterial::from_color(Color::BLACK))),
-    ));
+
+    let tile = Tile {
+        letter: Option::None,
+        color: Color::WHITE,
+    };
+
+    let game_state = GameState {
+        tiles: [[tile; 5]; 6],
+        current_row: 0,
+        current_index: 0,
+    };
+
+    for (row_index, row) in game_state.tiles.iter().enumerate() {
+        for (index, tile) in row.iter().enumerate() {
+            commands.spawn((
+                Mesh2d(meshes.add(Rectangle::new(TILE_SIZE, TILE_SIZE))),
+                MeshMaterial2d(materials.add(ColorMaterial::from_color(tile.color))),
+                Transform {
+                    translation: Vec3::new(
+                        (index as f32 * (TILE_SIZE + TILE_MARGIN)) - TRANSLATION_OFFSET_X,
+                        (row_index as f32 * (TILE_SIZE + TILE_MARGIN)) - TRANSLATION_OFFSET_Y,
+                        0.0,
+                    ),
+                    ..default()
+                },
+            ));
+        }
+    }
+}
+
+struct GameState {
+    tiles: [[Tile; 5]; 6],
+    current_row: u8,
+    current_index: u8,
+}
+#[derive(Clone, Copy)]
+struct Tile {
+    letter: Option<char>,
+    color: Color,
 }
