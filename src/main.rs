12dd69ecfdf0032881250f7aeb4f83648f853c07
William Woodhead <6097010+nofable@users.noreply.github.com>
2025-10-15 21:27:40 +0100

    initial commit with a black circle rendered on a simple UI window with a 2d camera

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2d);
+    commands.spawn((
+        Mesh2d(meshes.add(Circle::new(50.0))),
+        MeshMaterial2d(materials.add(ColorMaterial::from_color(Color::BLACK))),
+    ));
+}
